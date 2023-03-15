93ad3f75acd7bcac1f364ec322ffb8885d592b0c
MusaKolcuk <musa_kolcuk_1999@hotmail.com>
2023-03-15 17:33:00 +0300

    development

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 import  express  from "express";
+import dotenv from "dotenv"
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js"
+
+dotenv.config();
+
+//connection to the DB
+conn();
 
 const app = express();      //express uygulamasi olusturmak icin
 const port = 3000;
 
-app.get("/", (req, res) => {        // 3000 numarali porttan veri almak icin app.get yapılır.
-    res.send("INDEX SAYFASI");
-})
+// ejs template engine
+app.set("view engine", "ejs");
+
+
+// static dosyalarin sunulmasi icin middleware
+app.use(express.static('public'))
+app.use(express.json());
+
+//routes
+
+app.use("/", pageRoute);
+app.use("/photos", photoRoute);
+
+// app.get("/", (req, res) => {        // 3000 numarali porttan veri almak icin app.get yapılır.
+//     res.render("index");            // res.render express frameworkunde belirtilen sablon dosyalarinin islenmesini saglar.
+// });                                 // bu islemleri controllers ve routes dosyalarina ayristirarak yaptik.
+
+// app.get("/about", (req, res) => {
+//     res.render("about");
+// });
 
 
 app.listen(port, () => {
